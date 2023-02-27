b049b134399dba2ba06ec57015faea096f9ffaa3
CassioRaul <cassioraulcastro@gmail.com>
2023-02-26 22:30:46 -0300

    Integrando ao banco de dados

--- plans/models.py
+++ plans/models.py
@@ -1,9 +1,13 @@
 from django.db import models
 
 class Plans(models.Model):
-    title = models.CharField(max_length=120)
-    description = models.CharField(max_length=300)
-    type_plan = models.CharField(max_length=50)
-    road_map = models.CharField(max_length=300)
-    duration = models.IntegerField(default=0)
-    value = models.FloatField(default=0)    
+    titulo = models.CharField('TÍTULO', max_length=120)
+    description = models.CharField('DESCRIÇÃO', max_length=300)
+    type_plan = models.CharField('TIPO DE PLANO', max_length=50)
+    road_map = models.CharField('ROTEIRO', max_length=300)
+    duration = models.IntegerField('DURAÇÃO', default=0)
+    value = models.FloatField('PREÇO', default=0)
+
+    def __str__(self):
+        return f'{self.titulo}'
+    
